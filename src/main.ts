fbac0c2cb1e8d70cbde444f11e73907f30762e1a
Karol Sobolewski <karl.sobolewski@outlook.com>
2025-02-04 17:34:50 +0100

    0.0.1

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import Tag from "./components/Tag";
-import ToDo from "./toDo/ToDo";
 
 import "./main.css";
 
 // singleton
 class Root extends Tag {
   private static root: Root;
-  private todo: ToDo;
 
   private constructor() {
     // creating div tag
@@ -16,12 +14,6 @@ class Root extends Tag {
     this.setClass("root");
     this.render();
 
-    // TODO: rework anim class
-    // new Anim(this);
-
-    // adding as first child the todo app tag
-    this.todo = new ToDo();
-    this.addChild(this.todo.getElem());
   }
 
   // singleton check if exist and run constructor if need
--- src/toDo/toDoBody/bodyHandlers/BodyToDoButtonHandler.ts
+++
@@ -1,8 +0,0 @@
-import HandlerReadInput from "../../../components/handlers/HandlerReadInput";
-
-export default class BodyToDoButtonHandler extends HandlerReadInput {
-  func() {
-    // TODO: implement the function
-    console.log("JAVA");
-  }
-}
